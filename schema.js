0ec8f75e4e1c8194f9af3c97064f7d0bbf8feee8
dououFullstack <13412133311@139.com>
2018-03-14 17:34:31 +0800

    定义单个内容通过Id查询

--- schema.js
+++ schema.js
@@ -9,7 +9,7 @@ import axios from 'axios'
 const API_BASE = 'http://localhost:3001'
 
 const PostType = new GraphQLObjectType({
-  name: 'Posts',
+  name: 'Post',
   fields: {
     id: {
       type: GraphQLString
@@ -33,10 +33,24 @@ const QueryRootType = new GraphQLObjectType({
       type: GraphQLString,
       resolve: () => 'hello ~'
     },
+    // 定义列表查询
     posts: {
       type: new GraphQLList(PostType),
       resolve: () => {
         return axios.get(`${API_BASE}/posts`)
+          .then(response => response.data)
+      }
+    },
+    // 定义单个内容的查询
+    post: {
+      type: PostType,
+      args: {
+        id: {
+          type: GraphQLString
+        }
+      },
+      resolve: (obj, args, context) => {
+        return axios.get(`${API_BASE}/posts/${args.id}`)
           .then(response => response.data)
       }
     }
